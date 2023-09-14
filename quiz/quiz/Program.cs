b1c9ef523627a82a6a80b2c1df6ef107d2bce575
Remonit <scribb22m@gmail.com>
2023-09-14 15:15:52 +0200

    first push

---
+++ quiz/quiz/Program.cs
@@ -0,0 +1,74 @@
+﻿using System.Formats.Asn1;
+using System.Globalization;
+using System;
+using CsvHelper;
+using System.Numerics;
+using CsvHelper.Configuration;
+using System.Security.Cryptography.X509Certificates;
+
+namespace quiz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var reader = new StreamReader("D:/school/JAAR3/PROJECT/quiz/quiz/questions.csv"))
+            {
+
+                List<Question> questionList = new List<Question>();
+
+                while (!reader.EndOfStream)
+                {
+
+                    var line = reader.ReadLine();
+                    var values = line.Split(';');
+
+                    Question question = new Question(values[0], values[1], values[2], values[3], values[4], values[5]);
+                    questionList.Add(question);
+                }
+                questionList.Remove(questionList[0]);
+                foreach (var question in questionList)
+                {
+                    Console.WriteLine(question.Description);
+                    if(question.AnswerA == "" || question.AnswerB == "" || question.AnswerC == "")
+                    {
+                        Console.WriteLine("dit is een open vraag, vul je antwoord hieronder in:");
+                        string chosenAnswer = Console.ReadLine();
+                    }
+                    else
+                    {
+                        Console.WriteLine(
+                            $"\nA. {question.AnswerA}" +
+                            $"\nB. {question.AnswerB}" +
+                            $"\nC. {question.AnswerC}\n");
+                        string chosenAnswer = Console.ReadLine();
+                        if (CheckAnswer(question, chosenAnswer) == true)
+                        {
+                            Console.WriteLine("\nCORRECT!!\nje bent een fucking topper ofnie dan");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Helaas... het correcte antwoord was {question.CorrectAnswer.ToUpper()}");
+                        }
+                    }
+                    Console.WriteLine("\n\nklik op enter om verder te gaan");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+            }
+            
+        }
+        public static bool CheckAnswer(Question question, string chosenAnswer)
+        {
+            if(question.CorrectAnswer.ToUpper() ==  chosenAnswer.ToUpper())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
---
+++ quiz/quiz/Question.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quiz
+{
+    internal class Question
+    {
+        public string Id { get; set; }
+        public string Description { get; set; }
+        public string AnswerA {  get; set; }
+        public string AnswerB { get; set; }
+        public string AnswerC { get; set; }
+        public string CorrectAnswer {  get; set; }
+
+        public Question(string id, string description, string answerA, string answerB, string answerC, string correctAnswer)
+        {
+            Id = id;
+            Description = description;
+            AnswerA = answerA;
+            AnswerB = answerB;
+            AnswerC = answerC;
+            CorrectAnswer = correctAnswer;
+        }
+    }
+}
